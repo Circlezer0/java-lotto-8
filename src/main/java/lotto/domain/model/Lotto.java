2869331d3be9bd2cac07f3ff910c63430e7fa747
circlezero <wy129@naver.com>
2025-11-02 05:49:34 +0900

    refactor(Lotto) : 로또 예외 처리 리펙토링 - Validator 유틸 사용

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain.model;
 
 import java.util.List;
+import lotto.domain.utils.LottoNumberValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +12,9 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicate(numbers);
-        numbers.forEach(Lotto::validateNumberRange);
+        LottoNumberValidator.validateSize(numbers);
+        LottoNumberValidator.validateDuplicate(numbers);
+        numbers.forEach(LottoNumberValidator::validateRange);
     }
 
     public boolean contains(int number) {
@@ -22,29 +23,5 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
-    }
-
-    private static void validateSize(List<Integer> numbers) {
-        if (numbers == null) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호 리스트는 null일 수 없습니다.");
-        }
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    private static void validateNumberRange(Integer number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private static void validateDuplicate(List<Integer> numbers) {
-        long distinctCount = numbers.stream()
-                .distinct()
-                .count();
-        if(numbers.size() != distinctCount) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        }
     }
 }
