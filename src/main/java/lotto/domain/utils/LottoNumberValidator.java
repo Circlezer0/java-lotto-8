2f6080149ef2f79670426b0aee1035203b62948d
circlezero <wy129@naver.com>
2025-11-02 05:51:49 +0900

    feat(LottoNumberValidator) : 로또 검증 유틸 리펙토링 - LottoGameRule 활용

--- src/main/java/lotto/domain/utils/LottoNumberValidator.java
+++ src/main/java/lotto/domain/utils/LottoNumberValidator.java
@@ -3,33 +3,30 @@ package lotto.domain.utils;
 import java.util.List;
 
 public class LottoNumberValidator {
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
-    public static final int PICK_COUNT = 6;
 
     private LottoNumberValidator() {}
 
     public static void validateNumbers(List<Integer> numbers) {
-        LottoNumberValidator.validateSize(numbers);
+        validateSize(numbers);
         numbers.forEach(LottoNumberValidator::validateRange);
-        LottoNumberValidator.validateDuplicate(numbers);
+        validateDuplicate(numbers);
     }
 
     public static void validateWinning(List<Integer> numbers, int bonusNumber) {
-        LottoNumberValidator.validateNumbers(numbers);
+        validateNumbers(numbers);
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
     }
 
     public static void validateSize(List<Integer> numbers) {
-        if (numbers.size() != PICK_COUNT) {
+        if (numbers.size() != LottoGameRule.PICK_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
     public static void validateRange(int number) {
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+        if (number < LottoGameRule.MIN_NUMBER || number > LottoGameRule.MAX_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
