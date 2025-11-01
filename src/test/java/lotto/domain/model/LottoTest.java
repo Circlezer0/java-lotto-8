d63a12a2a4fe64e6ffb18841cc7786b958b850b0
circlezero <wy129@naver.com>
2025-11-01 23:35:10 +0900

    test(Lotto) : 로또 객체 테스트 코드 작성

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -1,11 +1,11 @@
 package lotto.domain.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
@@ -21,5 +21,54 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호가 특정 숫자를 가지고 있는지 확인")
+    void containsNumberTest() {
+        // Given
+        Lotto lotto = new Lotto(List.of(1, 12, 23, 34, 41, 45));
+
+        // When
+        int containsCount = 0;
+        for(int i = 1; i <= 45; i++) {
+            if(lotto.contains(i)) {
+                containsCount++;
+            }
+        }
+
+        // Then
+        assertEquals(6, containsCount);
+    }
+
+    @Test
+    @DisplayName("로또는 정렬된 상태로 저장된다.")
+    void lottoSortedTest() {
+        // Given
+        Lotto lotto = new Lotto(List.of(34, 12, 45, 1, 23, 41));
+        List<Integer> expectedOrder = List.of(1, 12, 23, 34, 41, 45);
+
+        // When
+        List<Integer> actualOrder = lotto.getNumbers();
+
+        // Then
+        assertEquals(expectedOrder, actualOrder);
+    }
+
+    @Test
+    @DisplayName("로또 번호 개수가 6개보다 작으면 예외가 발생한다.")
+    void numberCountLessThanSix() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    void numberOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
