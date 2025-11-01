45db85521369756bfa71f5236094a630281b8871
circlezero <wy129@naver.com>
2025-11-02 02:21:23 +0900

    feat(WinningNumber) : 당첨 번호 객체 구현

---
+++ src/main/java/lotto/domain/model/WinningNumber.java
@@ -0,0 +1,48 @@
+package lotto.domain.model;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public WinningNumber(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
+        this.numbers = numbers.stream().sorted().toList();
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        numbers.forEach(WinningNumber::validateNumberRange);
+        validateNumberRange(bonusNumber);
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 리스트는 null일 수 없습니다.");
+        }
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private static void validateNumberRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        long distinctCount = numbers.stream()
+                .distinct()
+                .count();
+        if(numbers.size() != distinctCount) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+}
