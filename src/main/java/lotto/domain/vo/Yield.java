e6d81bcad1fd421fd5172e4f7de62d6ed0b22b12
circlezero <wy129@naver.com>
2025-11-03 02:49:00 +0900

    refactor(Yield) : 예외 처리 변경

--- src/main/java/lotto/domain/vo/Yield.java
+++ src/main/java/lotto/domain/vo/Yield.java
@@ -2,6 +2,8 @@ package lotto.domain.vo;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import lotto.exception.LottoException;
+import lotto.exception.code.DomainErrorCode;
 
 public record Yield(BigDecimal percentage) {
     private static final BigDecimal MULTIPLIER = BigDecimal.valueOf(100);
@@ -12,7 +14,7 @@ public record Yield(BigDecimal percentage) {
 
     public static Yield from(long totalPrize, int spentMoney) {
         if (spentMoney <= 0) {
-            throw new IllegalArgumentException("[ERROR] 지출 금액이 최소값보다 작거나 같습니다.");
+            throw new LottoException(DomainErrorCode.INVALID_SPENT_MONEY);
         }
         BigDecimal prize = BigDecimal.valueOf(totalPrize);
         BigDecimal spent = BigDecimal.valueOf(spentMoney);
