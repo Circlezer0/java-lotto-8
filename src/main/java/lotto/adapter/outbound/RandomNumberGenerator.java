1ab23f7f22b17d20b64214965f26f04491fdc7b2
circlezero <wy129@naver.com>
2025-11-03 02:38:41 +0900

    refactor(RandomNumberGenerator) : 예외 처리 리펙토링

--- src/main/java/lotto/adapter/outbound/RandomNumberGenerator.java
+++ src/main/java/lotto/adapter/outbound/RandomNumberGenerator.java
@@ -3,21 +3,23 @@ package lotto.adapter.outbound;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import lotto.application.output.NumberGenerator;
+import lotto.exception.LottoException;
+import lotto.exception.code.AdapterErrorCode;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public List<Integer> uniqueNumbersInRange(int min, int max, int count) {
         if (min > max) {
-            throw new IllegalArgumentException("[ERROR] 최소값, 최대값 범위 오류입니다.");
+            throw new LottoException(AdapterErrorCode.INVALID_NUMBER_RANGE);
         }
 
         if (count < 0) {
-            throw new IllegalArgumentException("[ERROR] 생성할 숫자 개수는 음수일 수 없습니다.");
+            throw new LottoException(AdapterErrorCode.INVALID_NUMBER_COUNT);
         }
 
         if (min + count - 1 > max) {
-            throw new IllegalArgumentException("[ERROR] 범위 내에서 고유한 숫자를 생성할 수 없습니다.");
+            throw new LottoException(AdapterErrorCode.INSUFFICIENT_UNIQUE_NUMBERS);
         }
 
         return Randoms.pickUniqueNumbersInRange(min, max, count);
