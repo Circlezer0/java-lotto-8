c5e7c24d5a81478651e1878d4248b6a14f140ae8
circlezero <wy129@naver.com>
2025-11-02 18:50:29 +0900

    feat(Applicatiton) : 조립 로직 추가 및 컨트롤러 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.adapter.inbound.InputView;
+import lotto.adapter.inbound.LottoController;
+import lotto.adapter.inbound.OutputView;
+import lotto.adapter.outbound.RandomNumberGenerator;
+import lotto.application.input.LottoService;
+import lotto.application.output.NumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        LottoService lottoService = new LottoService(numberGenerator);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+
+        lottoController.run();
     }
 }
